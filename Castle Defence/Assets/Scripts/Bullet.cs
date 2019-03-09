0d2b95f650e109a9bfdd0a98cfea6e4e26208c27
Serhat <serhat@Serhats-MacBook-Pro.local>
2019-03-10 01:45:06 +0300

    First commit
    very first commit

---
+++ Castle Defence/Assets/Scripts/Bullet.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 10f;
+    public float damage = 10f;
+    public Vector3 direction;
+    public string targetTag;
+    private float ttl = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, ttl);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (direction != null)
+            transform.Translate(direction * Time.deltaTime * speed, Space.World);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == targetTag)
+        {
+            Destroy(gameObject);
+            if(collision.gameObject.GetComponent<Soldier>())
+            {
+                Soldier enemy = collision.gameObject.GetComponent<Soldier>();
+                enemy.health -= damage;
+                if (enemy.health <= 0)
+                {
+                    Destroy(collision.gameObject);
+                }
+            }
+            else
+            {
+                Castle enemy = collision.gameObject.GetComponent<Castle>();
+                enemy.health -= damage;
+                if (enemy.health <= 0)
+                {
+                    Destroy(collision.gameObject);
+                }
+            }
+        }
+    }
+
+}
---
+++ Castle Defence/Assets/Scripts/SoldierManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierManager : MonoBehaviour
+{
+
+    public GameObject soldierPrefab;
+    private GameObject soldier;
+
+    public float timeBetweenWaves = 5f;
+    private float countDown = 2f;
+    private int WaveIndex = 0;
+    private float waveSeparator = 0.5f; //sepearete instantiations in wave
+    private Transform spawnPoint;
+    private float randomizeSpawn = 3f; //for spawnwave to spawn different places
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPoint = this.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.tag == "TeamRed")
+        {
+            if (countDown <= 0f)
+            {
+                StartCoroutine(SpawnWave());
+                countDown = timeBetweenWaves;
+            }
+            countDown -= Time.deltaTime;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if(this.tag == "TeamBlue")
+        {
+            SpawnTeamBlueSoldier();
+        }
+    }
+
+    //make it coroutine to seperate instantiations from each other
+    IEnumerator SpawnWave()
+    {
+        WaveIndex++;
+        randomizeSpawn = Random.Range(-3f, 3f);
+        for (int i = 0; i < WaveIndex; i++)
+        {
+            SpawnTeamRedSoldier();
+            yield return new WaitForSeconds(waveSeparator);
+        }
+    }
+
+    void SpawnTeamRedSoldier()
+    {
+        soldier = Instantiate(soldierPrefab, new Vector2(spawnPoint.position.x, spawnPoint.position.y + randomizeSpawn), Quaternion.identity);
+        soldier.tag = "TeamRed";
+    }
+
+
+    void SpawnTeamBlueSoldier()
+    {
+        Vector3 mousePositionInWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        soldier = Instantiate(soldierPrefab, new Vector2(mousePositionInWorld.x, mousePositionInWorld.y), Quaternion.identity);
+        soldier.tag = "TeamBlue";
+    }
+}
