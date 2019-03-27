30b65ef7fb54c1890d8f3a7a5b14f638abb6c6f1
serhat <serhatdemir@gmail.com>
2019-03-27 12:55:27 +0300

    Campaigns scene fade in and fade out when loading new scene Credits page

---
+++ Assets/Scripts/SceneFader.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneFader : MonoBehaviour
+{
+    public Image fadeImage;
+
+    private void Start()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    public void FadeTo(string scene)
+    {
+        StartCoroutine(FadeOut(scene));
+    }
+
+    IEnumerator FadeIn()
+    {
+        float t = 1f;
+        Color tempColor = fadeImage.color;
+        
+        while (t > 0f)
+        {
+            t -= Time.deltaTime;
+            tempColor.a = t;
+            fadeImage.color = tempColor;
+            yield return 0;   //wait a frame
+        }
+    }
+    
+    IEnumerator FadeOut(string scene)
+    {
+        float t = 0f;
+        Color tempColor = fadeImage.color;
+        
+        while (t < 1f)
+        {
+            t += Time.deltaTime;
+            tempColor.a = t;
+            fadeImage.color = tempColor;
+            yield return 0;   //wait a frame
+        }
+
+        SceneManager.LoadScene(scene);
+    }
+}
